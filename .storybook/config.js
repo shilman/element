aa513eaa94a68a1861fb4a565fbdc56078360e64
Michael Shilman <shilman@lab80.co>
2017-07-23 16:41:27 -0700

    Try to add element (currently fails)

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,5 +1,10 @@
 import { configure } from '@storybook/vue'
 
+import Vue from 'vue';
+import Element from '../src/index.js';
+import '../packages/theme-default/src/index.css';
+Vue.use(Element);
+
 function loadStories() {
   require('../src/stories')
 }
